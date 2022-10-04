1426ddaa29228a712adb97fef59d663de36d9e4b
Junsang Mun <mun.js@aol.com>
2022-10-04 15:11:25 +0900

    practice @ import/export

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express');
+import express from 'express';
+import {area} from './square.js'
 const app = express();
 const port = 3000;
 
 app.get('/', function(req, res) {
-	res.send('Hello World!\n' + req);
+	res.send(`Hello World!\n${area(4)}`);
 });
 
 app.listen(port, function() {
 	console.log(`Now server is running on localhost:${port}`);
-})
+});
