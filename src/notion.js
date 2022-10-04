ea73ed1fa9b9ea3b7dffad02301792dddc4b22f9
Junsang Mun <mun.js@aol.com>
2022-10-04 20:40:01 +0900

    fail; also return 500
    - 이전에 retrive로 걸었을 때 500이라서 query로 변경해보았으나 이 역시 500

--- src/notion.js
+++ src/notion.js
@@ -4,15 +4,19 @@ dotenv.config();
 
 export function retrieveDatabase() {
 	const options = {
-		method: 'GET',
+		method: 'POST',
 		headers: {
 			accept: 'application/json',
+			'content-type': 'application/json',
 			'Notion-Version': '2022-06-28',
 			authorization: `Bearer ${process.env.NOTION_TOKEN}`
-		}
+		},
+		body: JSON.stringify({
+			page_size: 100
+		})
 	};
 
-	fetch(`https://api.notion.com/v1/databases/${process.env.NOTION_DATABASE_ID}`, options)
+	fetch(`https://api.notion.com/v1/databases/${process.env.NOTION_DATABASE_ID}/query`, options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.log(err));
