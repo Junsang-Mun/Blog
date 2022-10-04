fc36dcbe725eeb7085fab156e217a5b1b7e5a61e
Junsang Mun <mun.js@aol.com>
2022-10-04 23:10:19 +0900

    [feature] parse only title
    - now parse only titles; stored in postIDs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import { lookupDB } from './notion.js';
 const app = express();
 const port = 5173;
 
-app.get('/', function(req, res) {
-	const result = lookupDB();
-	res.send(`${result}`);
+app.get('/', async(req, res) => {
+	const result = await lookupDB();
+	res.send(`${result.results[0].properties}`);
+
+	const postIDs = result.results.map((post) => (
+		post.properties.title.title[0].plain_text
+	));
+	console.log(`postIDs: ${postIDs}`);
 });
 
 app.use(express.static('public'));
