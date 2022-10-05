becda46402c438ec380201d9c6d16b57d468f5b4
Junsang Mun <mun.js@aol.com>
2022-10-05 15:48:41 +0900

    [feature] add 4 routes in /api
    - /post: get post summarys
    - /post/id: get details of post(id)
    - /tag: show all of tags
    - /tag/id: show all of posts which contains tag(id)

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,14 +2,20 @@ import express from 'express';
 const router = express.Router();
 export { router as apiRouter }
 
-router.get("/", function(req, res, next) {
-	res.send('accessed to /api');
-});
-
-router.get("/post", function(req, res, next) {
+router.get('/post', function(req, res, next) {
 	res.send('accessed to /api/post');
 });
 
-router.get("/post/:id", function(req, res, next) {
+router.get('/post/:id', function(req, res, next) {
 	res.send('accessed to /api/post/id');
+	console.log(req.params.id);
+});
+
+router.get('/tag', function(req, res, next) {
+	res.send('accessed to /api/tag');
+});
+
+router.get('/tag/:id', function(req, res, next) {
+	res.send('accessed to /api/tag/id');
+	console.log(req.params.id);
 });
